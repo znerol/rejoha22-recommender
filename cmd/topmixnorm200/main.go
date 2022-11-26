2d68fcf76865e336a5bd7b82f7115b7cb4b48b7c
znerol <lo+github@znerol.ch>
2022-11-26 09:49:04 +0100

    feat(cli): Add cli util for mixed scoring

---
+++ cmd/topmixnorm200/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/znerol/rejoha22-recommender/popularity"
+)
+
+func main() {
+	source := popularity.NewPopularitySource()
+
+	log.Println("Attempting to scrape 200 most popular videos")
+	lines, err := source.Load(200)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Score\tPopularity\tCategory\tTitle\tId\n")
+	for _, line := range popularity.NewNormalizedPopularityList(lines, 1, 2) {
+		fmt.Printf("%f\t%f\t%s\t%s\t%s\n", line.Score, line.Popularity, line.Category, line.Title, line.Id)
+	}
+}
